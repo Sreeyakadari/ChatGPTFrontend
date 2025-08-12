f41e326ce7403bdd55847536d1ec5c7d27da7c4f
SreeyaKadari <sreeyakadari2024@gmail.com>
2025-08-12 16:52:30 -0500

    Updated code

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/ChatGPT/",
+  base: "/ChatGPTFrontend/",
   plugins: [react()],
   build: {
     outDir: "docs", // add this line
